53dd77e53d47df1ddccb910aa3311eb341c8445b
Ilxom00 <parkent05@gmail.com>
2026-08-10 14:08:20 +0500

    Add: backend/app/redis_client.py

---
+++ backend/app/redis_client.py
@@ -0,0 +1,7 @@
+import redis.asyncio as redis
+from app.config import settings
+
+redis_pool = redis.ConnectionPool.from_url(settings.redis_url, decode_responses=True)
+
+def get_redis():
+    return redis.Redis(connection_pool=redis_pool)
