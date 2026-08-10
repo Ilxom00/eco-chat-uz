ba92461da032d110a004d78a2bfde6a868c6b55c
Ilxom00 <parkent05@gmail.com>
2026-08-10 14:09:04 +0500

    Add: backend/app/models/__init__.py

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,8 @@
+from .base import Base
+from .admin import Admin
+from .branch import Branch
+from .employee import Employee
+from .topic import Topic
+from .question import Question, QuestionAnswer
+from .attempt import EmployeeTopicAssignment, EmployeeTopicQuestion, TestAttempt, AttemptQuestion
+from .audit import AuditLog
